102d1686c7a36f542fe2b048ef877168643102fc
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-12-13 20:48:55 -0500

    test

---
+++ src/main/java/com/brekaline/cartapp/CartAppApplication.java
@@ -0,0 +1,13 @@
+package com.brekaline.cartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brekaline/cartapp/CartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brekaline.cartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
